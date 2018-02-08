2bf72a54aa04e3be394c1d5f599e0420064d8ca3
Mayur Kapadia <lanetteam.mayur35.com>
2018-02-08 19:15:37 +0530

    first Commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var express=require('express');
+var app=express();
+var port=process.env.PORT||3001;
+var mongoose=require('mongoose');
+var passport=require('passport');
+var flash=require('connect-flash');
+
+var morgan=require('morgan');
+var cookieParser=require('cookie-parser');
+var bodyParser=require("body-parser");
+var session=require('express-session');
+var db=require('./config/database');
+
+mongoose.connect(db.url);
+
+require('./config/passport')(passport);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set('view Engine','html');
+
+//required for passport
+app.use(session({secret: 'mysecretkey'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./app/routes')(app,passport);
+
+app.listen(port);
+console.log("Connected to Server "+port);
